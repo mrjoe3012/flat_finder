da65c1da2c2970e0f5fb9bcc1095a11e26d1a7a5
Joseph Agrane <josephagrane@gmail.com>
2022-10-04 15:43:20 +0100

    modified filter to allow for 1 bedroom flats and no max

--- WebScanner.py
+++ WebScanner.py
@@ -33,27 +33,27 @@ class WebScanner(ABC):
 class RightMoveScanner(WebScanner):
     def __init__(self):
         super().__init__()
-        self.url = "https://www.rightmove.co.uk/property-to-rent/find.html?minBedrooms=2&maxBedrooms=2&keywords=&sortType=6&includeLetAgreed=false&viewType=LIST&channel=RENT&index=0&maxPrice=1000&radius=3.0&maxDaysSinceAdded=1&locationIdentifier=REGION^93616"
+        self.url = "https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION%5E93616&maxBedrooms=10&minBedrooms=1&maxPrice=1000&radius=3.0&propertyTypes=&maxDaysSinceAdded=1&includeLetAgreed=false&mustHave=&dontShow=&furnishTypes=&keywords="
         self.base_url = "https://rightmove.co.uk"
         self.re1 = 'propertyCard-link.*href="([^"]+)"'
 
 class ZooplaScanner(RightMoveScanner):
     def __init__(self):
         super().__init__()
-        self.url = "https://www.zoopla.co.uk/to-rent/property/2-bedrooms/glasgow-city-centre/?price_frequency=per_month&price_max=1000&q=Glasgow+City+Centre%2C+Glasgow&radius=3&search_source=refine&beds_min=2&added=24_hours"
+        self.url = "https://www.zoopla.co.uk/to-rent/property/2-bedrooms/glasgow-city-centre/?price_frequency=per_month&price_max=1000&q=Glasgow+City+Centre%2C+Glasgow&radius=3&search_source=refine&beds_min=1&added=24_hours&view_type=list"
         self.base_url = "https://zoopla.co.uk"
         self.re1 = 'href="(\/to-rent\/details\/[0-9]+\/[^"]+)"'
 
 class OnTheMarkerScanner(RightMoveScanner):
     def __init__(self):
         super().__init__()
-        self.url = "https://www.onthemarket.com/to-rent/2-bed-flats-apartments/glasgow-west-end/?max-price=1000&radius=4.0&recently-added=24-hours"
+        self.url = "https://www.onthemarket.com/to-rent/1-bed-flats-apartments/glasgow-west-end/?max-price=1000&radius=4.0&recently-added=24-hours"
         self.base_url = "https://onthemarket.com"
         self.re1 = 'href="(\/details\/[^"]+)"'
 
 class S1HomesScanner(RightMoveScanner):
     def __init__(self):
         super().__init__()
-        self.url = "https://www.s1homes.com/rent/search/forrent_search_results.cgi?refine=1&verylocalstart=&area_id=&sortedby=&location=7&minprice=0&maxprice=1000&bedrooms=2&bedroomsMin=2&bedroomsMax=&type=Flat&availability=Any&furnished=&whenpropadded=1&keywords=&submit=&all="
+        self.url = "https://www.s1homes.com/rent/search/forrent_search_results.cgi?refine=1&verylocalstart=&area_id=&sortedby=&location=&minprice=0&maxprice=1000&bedrooms=1-3&bedroomsMin=1&bedroomsMax=3&type=Flat&availability=Any&furnished=&whenpropadded=1&keywords=&submit=&all="
         self.base_url = "https://www.s1homes.com"
         self.re1 = 'href="(\/Flats-for-rent\/[^"]+\.shtml)"'
